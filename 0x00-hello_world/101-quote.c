2312813e500ebb8800e37ecfc7c0d33e4fc53b1e
calebonyedikachi <calebugwoke@gmail.com>
2022-12-09 18:22:59 -0800

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-    #include <unistd.h>                                                                                                                      
-/** 
+
+#include <unistd.h>
+/**
  * main - Entry
  * Return: Always 1
 */
@@ -9,3 +10,4 @@ int main(void)
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
